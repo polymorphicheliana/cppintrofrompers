741f17a8784d261541793b2833f2503c1b7ecb60
Iliana <pinkiliana@proton.me>
2026-01-03 16:42:39 +0200

    created random number generator for 0 - 9

--- project1.c
+++ project1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello world!!!\n");
+    srand(time(NULL));
+    int x = rand() % 10;
+    printf("%d\n", x);
     return 0;
 }
