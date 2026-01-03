d47ae0857f4d793759aef5e3173de264951b2b1a
Iliana <pinkiliana@proton.me>
2026-01-03 17:17:08 +0200

    created a guesing game

--- project1.c
+++ project1.c
@@ -6,6 +6,16 @@ int main()
 {
     srand(time(NULL));
     int x = rand() % 10;
-    printf("%d\n", x);
+    int y;
+    scanf("%d", &y);
+    if (x == y)
+    {
+        printf("success\n");
+    }
+    else 
+    {
+        printf("the nump you chose was %d it was incorrect\n", y);
+        printf("the correct nump was %d\n", x);
+    }
     return 0;
 }
